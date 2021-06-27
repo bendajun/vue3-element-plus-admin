719857d01c538105114afa81fdb45b808adb9023
dudapeng <1442766925@qq.com>
2021-06-27 18:47:08 +0800

    docs: 去除无用内容

--- src/views/error/404.vue
+++ src/views/error/404.vue
@@ -5,18 +5,7 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
 
-export default defineComponent({
-  watch: {
-    $route: {
-      immediate: true,
-      handler(val) {
-        console.log(val)
-      }
-    }
-  },
-})
 /*
   当访问一个不存在的路径的时候，如/is/does/not/exist，会跳转到当前页面，路由信息如下
   fullPath: "/is/does/not/exist"
